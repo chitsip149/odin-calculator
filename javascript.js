536ca263ce3334d05a4e845cedd1fe047968634d
chitsip149 <minhtrangkl03@gmail.com>
2025-04-21 20:04:36 +0800

    Add the logic for all the buttons

--- javascript.js
+++ javascript.js
@@ -15,10 +15,10 @@ function divide (a, b){
 }
 
 function operate(op, a, b){
-    if (op=='+') return add(a, b);
-    if (op=='-') return subtract(a, b);
-    if (op=='*') return multiply(a, b);
-    if (op=='/') return divide(a, b);
+    if (op=="add") return add(a, b);
+    if (op=="subtract") return subtract(a, b);
+    if (op=="multiply") return multiply(a, b);
+    if (op=="divide") return divide(a, b);
 }
 
 let replace = true;
@@ -46,3 +46,53 @@ digitButtons.forEach(btn => {
         }
     })
 })
+
+
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        operand1 = Number(display.textContent);
+        operator = e.target.id;
+        replace = true;
+    })
+})
+
+const equalButton = document.querySelector("#equal");
+equalButton.addEventListener("click", () => {
+    if (operator!=undefined){
+        operand2=Number(display.textContent);
+        let result = operate(operator, operand1, operand2);
+        display.textContent = result;
+
+    }
+    operand1=undefined;
+    operand2=undefined;
+    operator=undefined;
+    replace=true;
+})
+
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", () => {
+    display.textContent="0";
+    operand1=undefined;
+    operand2=undefined;
+    operator=undefined;
+    replace=true;
+})
+
+function changeSign(num){
+    if (num==0) return num;
+    let returnedNum = (-Number(num)).toString();
+    return returnedNum;
+}
+
+const unaryButton = document.querySelector("#unary-operator");
+unaryButton.addEventListener("click", ()=> {
+    display.textContent = changeSign(display.textContent);
+})
+
+const percentButton = document.querySelector("#percent");
+percentButton.addEventListener("click", () => {
+    display.textContent = display.textContent / 100;
+})
+

46ae88d18c79c4982fc8e2c4dda16b513002f2b1
chitsip149 <minhtrangkl03@gmail.com>
2025-04-21 19:38:54 +0800

    Complete the style and logic for digit buttons

--- javascript.js
+++ javascript.js
@@ -21,16 +21,28 @@ function operate(op, a, b){
     if (op=='/') return divide(a, b);
 }
 
+let replace = true;
+let operator;
+let operand1;
+let operand2;
+
 const display = document.querySelector("#display");
 
 const digitButtons = document.querySelectorAll(".digit");
 digitButtons.forEach(btn => {
     btn.addEventListener("click", (e)=>{
-        if (display.textContent!="0") {
-            display.textContent += e.target.textContent;
+        if (e.target.id=="dot"){
+            if (!display.textContent.split('').includes('.')){
+                display.textContent+=e.target.textContent;
+            }
+            replace=false;
+        }
+        else if (replace) {
+            display.textContent = e.target.textContent;
+            replace = false;
         }
         else {
-            display.textContent = e.target.textContent;
+            display.textContent += e.target.textContent;
         }
     })
 })
